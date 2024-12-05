2d38148ecc040c8571215ac2a9298c785924ef28
Sathish M <153342012+SathishTeckas@users.noreply.github.com>
2024-12-05 17:14:39 +0530

    Update ChatBot.js

--- ChatBot.js
+++ ChatBot.js
@@ -120,7 +120,7 @@
 
     // Add iframe to chatbot container
     const iframe = document.createElement('iframe');
-    iframe.src = `https://affc-2409-408d-1eca-4047-a8e1-c146-606c-8803.ngrok-free.app?agentId=${agentId}&accountId=${accountId}`;
+    iframe.src = `https://invoicing-ai-agent.vercel.app/?agentId=${agentId}&accountId=${accountId}`;
     iframe.title = 'Chatbot';
     chatbotContainer.appendChild(iframe);
 
